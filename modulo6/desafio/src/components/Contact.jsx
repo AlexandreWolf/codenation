844f07d3d47b5d7654a4f8968cc00bcf605d011d
Alexandre Guidio Wolf <alexandregw1@gmail.com>
2020-05-18 17:08:51 -0300

    Adicionando códigos do desafio do módulo 6

---
+++ modulo6/desafio/src/components/Contact.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import formatDate from "../utils/formatDate";
+const Contact = ({ data }) => {
+
+  const {
+    avatar,
+    name,
+    phone,
+    country,
+    admissionDate,
+    company,
+    department,
+  } = data;
+  
+  return (
+    <article className="contact" data-testid="contact">
+      <span className="contact__avatar" data-testid="contact-avatar">
+        <img src={avatar} alt={name} />
+      </span>
+
+      <span className="contact__data" data-testid="contact-name">
+        {name}
+      </span>
+
+      <span className="contact__data" data-testid="contact-phone">
+        {phone}
+      </span>
+
+      <span className="contact__data" data-testid="contact-country">
+        {country}
+      </span>
+
+      <span className="contact__data" data-testid="contact-date">
+        {admissionDate ? formatDate(admissionDate) : ""}
+      </span>
+
+      <span className="contact__data" data-testid="contact-company">
+        {company}
+      </span>
+
+      <span className="contact__data" data-testid="contact-department">
+        {department}
+      </span>
+    </article>
+  );
+};
+
+export default Contact;
